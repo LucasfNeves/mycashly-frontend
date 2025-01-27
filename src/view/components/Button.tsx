68abcf2b3a34b18a7edadbcc55f58907f2b012ea
Lucas Farias <lucasfarias.ln@gmail.com>
2025-01-27 19:57:44 -0300

    feat: add Loading satge and spinner component

--- src/view/components/Button.tsx
+++ src/view/components/Button.tsx
@@ -1,22 +1,31 @@
 import { ComponentProps } from 'react'
 import { cn } from '../../app/utils/cn'
+import { Spinner } from './Spinner'
 
 interface ButtonProps extends ComponentProps<'button'> {
   className?: string
   disabled?: boolean
   children: string
+  isLoading?: boolean
 }
 
-export function Button({ children, className, disabled }: ButtonProps) {
+export function Button({
+  children,
+  className,
+  disabled,
+  isLoading,
+  ...props
+}: ButtonProps) {
   return (
     <button
-      disabled={disabled}
+      disabled={disabled || isLoading}
       className={cn(
-        'h-[3.25rem] rounded-md bg-primaryBlue-500 text-white hover:opacity-70 hover:transition-all hover:duration-300 disabled:cursor-not-allowed disabled:bg-primaryBlue-100 disabled:text-gray-600',
+        'className={`flex hover:disabled:transition-none`} h-[3.25rem] items-center justify-center rounded-md bg-primaryBlue-500 font-medium text-white hover:bg-primaryBlue-600 hover:opacity-70 hover:transition-all hover:duration-300 disabled:cursor-not-allowed disabled:bg-gray-200 disabled:text-gray-400 hover:disabled:bg-gray-200 hover:disabled:opacity-100',
         className,
       )}
+      {...props}
     >
-      {children}
+      {isLoading ? <Spinner className="h-6 w-6" /> : children}
     </button>
   )
 }
