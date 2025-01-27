cd4ab4debd119d22f7f84cfc0396d5cfaf34ffbe
Lucas Farias <lucasfarias.ln@gmail.com>
2025-01-27 19:53:46 -0300

    feat: add react query provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import { Toaster } from 'sonner'
+import { AuthProvider } from './context/AuthContext'
 import { Router } from './router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: false,
+      refetchOnWindowFocus: false,
+    },
+  },
+})
 
 export function App() {
-  return <Router />
+  return (
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <Router />
+        <Toaster />
+      </AuthProvider>
+    </QueryClientProvider>
+  )
 }
