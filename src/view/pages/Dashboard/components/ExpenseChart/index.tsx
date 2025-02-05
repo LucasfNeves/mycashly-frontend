17c167ecfa2d1cc7e61e157a7568596c4b8c6f05
Lucas Farias <lucasfarias.ln@gmail.com>
2025-02-05 15:30:50 -0300

    feat: rename shadcn path

--- src/view/pages/Dashboard/components/ExpenseChart/index.tsx
+++ src/view/pages/Dashboard/components/ExpenseChart/index.tsx
@@ -3,7 +3,7 @@ import {
   ChartContainer,
   ChartTooltip,
   ChartTooltipContent,
-} from '@/view/shadCnComponents/ui/chart'
+} from '@/view/shadcn/ui/chart'
 
 import { Bar, BarChart, XAxis, YAxis, LabelList } from 'recharts'
 
