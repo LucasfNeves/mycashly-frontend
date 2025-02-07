4fb6ca561218ce98b6ea41eaf5cdeda0d142089b
Lucas Farias <lucasfarias.ln@gmail.com>
2025-02-07 17:20:46 -0300

    refactor: dayPicker popover from shadcn to radix ui and DayPicker

--- src/view/components/DatePickerInput/index.tsx
+++ src/view/components/DatePickerInput/index.tsx
@@ -1,13 +1,10 @@
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from '@/components/ui/popover'
 import { DatePicker } from '../DatePicker'
 import { forwardRef } from 'react'
 import { formatDate } from '@/app/utils/formatDate'
 import { useDataPickerInputController } from './useDatePickerInputController'
 import { InputMensagerError } from '../inputMensagerError'
+import { Popover } from '../Popover'
+import { cn } from '@/app/utils/cn'
 
 interface DatePickerInputProps {
   className?: string
@@ -26,29 +23,31 @@ export const DatePickerInput = forwardRef<
   })
 
   return (
-    <div className="relative flex w-full flex-col gap-2">
-      <Popover>
-        <PopoverTrigger
-          className="relative flex flex-col gap-2 overflow-hidden"
-          ref={ref}
-        >
-          <div className="relative h-[3.25rem] w-full rounded-lg border-2 border-neutral-200 bg-transparent px-[12px] pb-[2px] pt-5 text-start text-neutral-200 outline-none placeholder-shown:pt-0">
-            {formatDate(new Date(selectedDate))}
-          </div>
+    <div className="relative flex flex-col gap-2">
+      <Popover.Root>
+        <Popover.Trigger>
+          <button
+            ref={ref}
+            className={cn(
+              'relative h-[3.25rem] rounded-lg border-2 border-neutral-200 bg-transparent px-[12px] pb-[2px] pt-5 text-start text-neutral-200 outline-none placeholder-shown:pt-0',
+            )}
+          >
+            <span className="pointer-events-none absolute left-4 top-2 text-xs text-neutral-200 transition-all">
+              Data
+            </span>
 
-          <label className="pointer-events-none absolute left-4 top-2 text-xs text-neutral-200 transition-all">
-            Data
-          </label>
-        </PopoverTrigger>
-        <PopoverContent className="w-fit border-none bg-white p-0">
+            <span>{formatDate(new Date(selectedDate))}</span>
+          </button>
+        </Popover.Trigger>
+        <Popover.Content side="top" className="w-fit border-none bg-white p-0">
           <DatePicker
             value={new Date(selectedDate)}
             onChange={(date) => {
               handleDateChange(date)
             }}
           />
-        </PopoverContent>
-      </Popover>
+        </Popover.Content>
+      </Popover.Root>
 
       {error && <InputMensagerError error={error} />}
     </div>
---
+++ src/view/components/Popover/index.ts
@@ -0,0 +1,7 @@
+import { PopoverContent, PopoverRoot, PopoverRTrigger } from './popover'
+
+export const Popover = {
+  Root: PopoverRoot,
+  Trigger: PopoverRTrigger,
+  Content: PopoverContent,
+}
