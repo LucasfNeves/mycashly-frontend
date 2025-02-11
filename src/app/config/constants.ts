cc98eb529a8dc169c93730094d8ee0da98747660
Lucas Farias <lucasfarias.ln@gmail.com>
2025-02-11 14:51:41 -0300

    fix: delete Trasactions mocked

--- src/app/config/constants.ts
+++ src/app/config/constants.ts
@@ -1,5 +1,3 @@
-import { TransactionDetails } from '../entities/TransactionDetails'
-
 export const MONTHS = [
   'Jan',
   'Fev',
@@ -19,39 +17,4 @@ export const TYPES = [
   { value: 'INCOME', label: 'Receita' },
   { value: 'EXPENSE', label: 'Despesa' },
   { value: 'INVESTMENT', label: 'Investimento' },
-]
-
-export const transactionsMocked = [
-  {
-    id: '1',
-    name: 'Restaurante',
-    category: 'investment',
-    date: '2025-01-04T23:11:01.677Z',
-    type: 'expense',
-    value: 110,
-  } as TransactionDetails,
-  {
-    id: '2',
-    name: 'Salário',
-    category: 'investment',
-    date: '2025-01-04T23:11:01.677Z',
-    type: 'income',
-    value: 1.5,
-  } as TransactionDetails,
-  {
-    id: '3',
-    name: 'Investimento',
-    category: 'investment',
-    date: '2025-01-04T23:11:01.677Z',
-    type: 'investment',
-    value: 500,
-  } as TransactionDetails,
-  {
-    id: '4',
-    name: 'investment',
-    category: 'Alimentação',
-    date: '2025-01-04T23:11:01.677Z',
-    type: 'expense',
-    value: 110,
-  } as TransactionDetails,
 ]
